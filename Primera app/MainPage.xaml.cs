3b8ff3d4551971560218be2fb1e3ab1ef0ef3c34
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-03-05 18:43:16 -0600

    Add files via upload

---
+++ Primera app/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Primera_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Inicializar();
+
+        }
+
+        private void Inicializar()
+        {
+            pro1.Clicked += Pro1_Clicked;
+            pro2.Clicked += Pro2_Clicked;
+
+        }
+
+        private void Pro2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new pro2());
+        }
+
+        private void Pro1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new pro1());
+        }
+    }
+}
---
+++ Primera app/cal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Primera_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class cal : ContentPage
+    {
+        public cal()
+        {
+            InitializeComponent();
+            pic.Items.Add("Multiplicación");
+            pic.Items.Add("suma");
+
+
+        }
+
+        private void pic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string op = pic.SelectedItem.ToString();
+            int n1 = Int32.Parse(num1.Text);
+            int n2 = Int32.Parse(num2.Text);
+            double res = 0;
+
+            if (op == "Suma") 
+            res = n1 + n2;
+            else if (op == "Multiplicación") ;
+            res = n1 * n2;
+            lares.Text = res + "";
+
+
+        }
+    }
+}
